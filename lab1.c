2446c391b5ba7bda31c150971add732fa59c7ca0
alunos <alunos@douro.ist.utl.pt>
2015-02-25 09:28:40 +0000

    parte 3 e 4 do lab

--- lab1.c
+++ lab1.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <netdb.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
@@ -12,18 +13,45 @@
 extern int errno;
 
 int main(void)
-{
+{	
 	struct hostent *h;
 	struct in_addr *a;
+	struct sockaddr_in addr;
+	int fd,n,addrlen;
+	char buffer[128];
 
-	if((h = gethostbyname("www.google.com"))==NULL)
+	if((h = gethostbyname("tejo"))==NULL)
+		exit(1);
+
+	a = (struct in_addr*)h->h_addr_list[0];
+	
+	fd=socket(AF_INET,SOCK_DGRAM,0);
+	if(fd==-1)
 	{
 		exit(1);
 	}
 
-	printf("official host name: %s\n", h->h_name);
+	addrlen=sizeof(addr);
+	
+	memset((void*)&addr,(int)'\0',addrlen);
 
-	a = (struct in_addr*)h->h_addr_list[0];
-	printf("internet address: %s (%08lX)\n", inet_ntoa(*a), (long unsigned int)ntohl(a->s_addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr = *a;
+	addr.sin_port = htons(8000);
+	
+	n=sendto(fd,"Hello!\n",7,0,(struct sockaddr*)&addr,addrlen);
+	
+	if(n==-1) exit(1);
+	
+	
+	n=recvfrom(fd,buffer,128,0,(struct sockaddr *)&addr,&addrlen);
+
+	if(n==-1) exit(1);
+
+	write(1,"echo: ",6);
+	write(1,buffer,n);
+
+	close(fd);
 	exit(0);
+	
 }
