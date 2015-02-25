883ba26e0ca8a70aeeb5a34a1aebf22ee5017d59
Nuno Mendes <gugas93@hotmail.com>
2015-02-25 21:21:41 +0000

    Added the getopt funcionality, doesn't give errors but the program doesn't run

--- ddt.c
+++ ddt.c
@@ -1,43 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
 
 
-int main(int argc, char argv)
+int main(int argc, char **argv)
 {
-  //bootIP and bootport are the IP adress e UDP port of the starting server.
+  //bootIP and bootport are the IP adress e UDP port of the starting server
 
   //Ringport is the TCP server port used for establishing a TCP session in the ring
 
-  char bootIP[1000];
-  int bootport,ringport,i;
+  char* bootIP;
+  int bootport,ringport,option;
 
   //By default, bootIP="tejo.ist.utl.pt" and bootport=58000
-  bootIP="tejo.ist.utl.pt/0";
+  bootIP="tejo.ist.utl.pt\0";
   bootport=58000;
-  i=1;
 
-
-  while(i<argc)
-  {
-    switch(argv[i])
+  /*if(argc % 2 != 0)
     {
-      case "-t":
-        ringport=argv[i+1];
+      printf("The function doesn't have enough arguments or it has more arguments than it can handle.\n")
+      exit(1);
+    }*/
+
+  while((option = getopt(argc,argv,"t:i:p:")) != -1) // Checking the various options received on the console application
+  {
+    switch(option)
+    {
+      case 't':
+        ringport = atoi(optarg); // Convert the optarg from the string format to integer type
       break;
 
-      case "-i":
-        bootIP=argv[i+1];
+      case 'i':
+        bootIP = optarg;
       break;
 
-      case "-p":
-        bootport=argv[i+1];
+      case 'p':
+        bootport = atoi(optarg);
       break;
 
       default:
-        printf("The expected arguments don't have the proper format. Application Aborted.");
-        exit(1);
+        printf("The expected arguments don't have the proper format. Application Aborted.\n");
+        exit(2);
     }
-    i+=2;
+
   }
 
 
