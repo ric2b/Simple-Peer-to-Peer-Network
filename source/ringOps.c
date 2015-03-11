bb8ea35c7b4dd65f272d70d349c4e851dd9ccd99
Nuno Mendes <gugas93@hotmail.com>
2015-03-11 09:33:56 +0000

    Solved a lot of issues on the removeNode

--- source/ringOps.c
+++ source/ringOps.c
@@ -6,143 +6,58 @@
 #include "ringOps.h"
 #include "network.h"
 
-
-
-/*
-socketStruct setupListenSocket(char * myIP, int myPort)
-{
-  socketStruct ListenSocket;
-
-  ListenSocket = setupSocket("NULL", 0, 'T');
-
-  if(listen(ListenSocket.socketFD, MAXPENDING) != 0)
-  {
-    printf("erro a fazer listen\n");
-    exit(-1);
-  }
-
-  bzero(ListenSocket.addr, ListenSocket.addrlen);
-  ListenSocket.addr->sin_family = AF_INET;
-  ListenSocket.addr->sin_addr.s_addr = htonl(INADDR_ANY);
-  ListenSocket.addr->sin_port = htons(myPort);
-
-  if(bind(ListenSocket.socketFD, (struct sockaddr *) ListenSocket.addr, ListenSocket.addrlen) < 0)
-  {
-    printf("erro a fazer bind\n");
-    exit(-1);
-  }
-
-  gethostname(myIP, 128);
-
-  return ListenSocket;
-}
-*/
-
-
 int removeNode(ringStruct * ringData, socketStruct socketCFG, socketStruct succiPeer, socketStruct prediPeer)
 {
   //getting out with return 0, everything went fine
   //getting out with return 1, something went wrong
-
-  char msg[128], cmd[10], idIP[30], buffer[128];
-  int problem, ringx, ringID, startTCP;
-
-  // If the node is unique remove the ring from the server
-
-  if(ringData.myID == ringData.succiID && ringData.succiID == ringData.prediID)
+  char msg[128], buffer[128];
+  if(ringData.succiID == -1 && ringData.prediID == -1) // If the node is unique remove the ring from the server
   {
-    sprintf(msg,"UNR %i\n",ringData.ringID); //check if an /n is necessary!
-
-    if((problem = sendUDP(msg,strlen(msg),socketCFG)) == -1)
-    {
-      exit(1);
-    }
-    if((problem = recvUDP(buffer,socketCFG)) == -1)
-    {
-      exit(1);
-    }
+    sprintf(msg,"UNR %i\n",ringData.ringID);
+    if(sendUDP(msg,strlen(msg),socketCFG) == -1)
+      return 1;
+    if(recvUDP(buffer,socketCFG) == -1)
+      return 1;
     else if(strcmp(buffer,"OK")==0)
-    {
-      exit(0);
-    }
+      return 0;
     else
-    {
-      exit(1);
-    }
+      return 1;
   }
   else
   {
-
-  // Test to check if the current node is the starter node. If it is, put the next node as the starter one
-
-    sprintf(msg,"BQRY %i\n",ringData.ringID);
-
-    if((problem = sendUDP(msg,strlen(msg),socketCFG)) == -1)
-      exit(1);
-
-    if((problem = recvUDP(buffer,socketCFG)) == -1)
+    if(ringData.starter == 1) // Test to check if the current node is the starter node. If it is, put the next node as the starter one
     {
-      exit(1);
-    }
-    else if(sscanf(buffer,"%s %i %i %s %i", cmd, &ringx, &ringID, idIP, &startTCP) == 5 && strcmp(cmd,"BRSP") == 0 && ringID == ringData.myID)
-    {
-      memset(buffer,0,strlen(buffer));
-      memset(msg,0,strlen(msg));
-
       sprintf(msg,"REG %i %i %s %i\n", ringData.ringID, ringData.succiID, ringData.succiIP, ringData.succiPort);
-
-      if((problem = sendUDP(msg,strlen(msg),socketCFG)) == -1)
-        exit(1);
-
-      if((problem = recvUDP(buffer,socketCFG)) == -1)
-      {
-        exit(1);
-      }
+      if(sendUDP(msg,strlen(msg),socketCFG) == -1)
+        return 1;
+      if(recvUDP(buffer,socketCFG) == -1)
+        return 1;
       else if(strcmp(buffer,"OK")==0)
       {
-
         memset(msg,0,strlen(msg));
-
         sprintf(msg,"BOOT\n");
-
         sendTCP(msg, strlen(msg), succiPeer);
-
         closeSocket(succiPeer);
-
         memset(msg,0,strlen(msg));
-
         sprintf(msg,"CON %i %s %i\n", ringData.succiID, ringData.succiIP, ringData.succiPort);
-
         sendTCP(msg, strlen(msg), prediPeer);
-
         closeSocket(prediPeer);
-
-        exit(0);
+        return 0;
       }
-
-      // Default Case
-
-      else
+      else // Default Case
       {
         closeSocket(succiPeer);
-
         memset(msg,0,strlen(msg));
-
         sprintf(msg,"CON %i %s %i\n", ringData.succiID, ringData.succiIP, ringData.succiPort);
-
         sendTCP(msg, strlen(msg), prediPeer);
-
         closeSocket(prediPeer);
-
-        exit(0);
+        return 0;
       }
     }
     else
-    {
-      exit(1);
-    }
+      return 1;
   }
-  exit(1);
+  return 1;
 }
 
 int searchNode(ringStruct * ringData, socketStruct succiPeer, int k)
@@ -165,9 +80,9 @@ int searchNode(ringStruct * ringData, socketStruct succiPeer, int k)
 
     printf("%i %s %i", qryID, qryIP, qryTCP);
 
-    exit(0);
+    return 0;
   }
-  exit(1);
+  return 1;
 }
 
 /*
